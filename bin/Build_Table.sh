27b3169c0213ecb9a76f09ec4fd700301fb50a3c
jingqian05 <31260963+jingqian05@users.noreply.github.com>
2017-09-18 19:15:27 -0700

    Update Build_Table.sh

--- bin/Build_Table.sh
+++ bin/Build_Table.sh
@@ -58,5 +58,5 @@ if [ ! -f $GTF ]; then
    $(gunzip $f);
    echo "file $f is unzipped as and should be ${GTF}"
 fi
-(awk '$3 ~ /gene/ { print $2,$1,$4,$5}' Homo_sapiens.GRCh37.75.gtf | sed 's/ /,/g' | sed -e '1ifeature,chr,start,end'> gene_dist_head.csv)
+(grep -v "^#" Homo_sapiens.GRCh37.75.gtf | awk '{ print $2,$3,$1,$4,$5}' | sed 's/ /\t/g' | sed -e '1itranscript_type\tfeature\tchr\tstart\tend'> gene_dist_head.tsv)
 exit 0;
